a1269a4f2a8b5418cb80e3d9124b6685c2c436ae
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2019-12-25 12:26:41 +0100

    Proper test setup for test containers

--- readme.md
+++ readme.md
@@ -7,10 +7,12 @@ in:  [Spring Cloud Gateway](https://cloud.spring.io/spring-cloud-gateway/referen
 
 Currently you may find here examples of: 
 1. Circuit Breaker and Fallback with [resilience4j](https://resilience4j.readme.io/docs/getting-started) - the detailed explanation is my on blog :
+2. How to mock http service using mock server test containers for micro-services integration tests
+3. How to configure externally in spring config Resilience4j circuit breakers and integrate it with Spring cloud circuit breaker starter
 
 ### Usage 
 1. To build and run the main application you need to have Maven, JDK8+ and Docker.
 2. To build it run command `mvn clean install`
-3. During Maven build the JUnit integration tests are running. They are using [Testcontainers](https://www.testcontainers.org/) for mocking downstream service.
+3. During Maven build the JUnit integration tests are running. We are using [Testcontainers](https://www.testcontainers.org/) for mocking downstream service.
 
-Feel free to propose your code changes ! 
+Feel free to propose your code changes / extensions ! 
--- src/test/java/io/github/romeh/services/gateway/GatewayCircuitBreakerTest.java
+++ src/test/java/io/github/romeh/services/gateway/GatewayCircuitBreakerTest.java
@@ -1,9 +1,14 @@
 package io.github.romeh.services.gateway;
 
 
-import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
-import com.carrotsearch.junitbenchmarks.BenchmarkRule;
-import org.junit.*;
+import static org.mockserver.model.HttpResponse.response;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockserver.client.server.MockServerClient;
@@ -13,71 +18,87 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.MockServerContainer;
+
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+
 import io.github.romeh.services.gateway.controller.FallbackResponse;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
-import static org.mockserver.model.HttpResponse.response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
+@ContextConfiguration(initializers = {GatewayCircuitBreakerTest.Initializer.class})
 public class GatewayCircuitBreakerTest {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayCircuitBreakerTest.class);
+	private static MockServerContainer mockServerContainer;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(GatewayCircuitBreakerTest.class);
+	static {
+		mockServerContainer = new MockServerContainer();
+		mockServerContainer.start();
 
-    @Rule
-    public TestRule benchmarkRun = new BenchmarkRule();
+	}
 
-    @ClassRule
-    public static MockServerContainer mockServer = new MockServerContainer();
+	static class Initializer
+			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.cloud.gateway.routes[0].id=test-service-withResilient4j",
+					"spring.cloud.gateway.routes[0].uri=" + mockServerContainer.getEndpoint(),
+					"spring.cloud.gateway.routes[0].predicates[0]=" + "Path=/testService/**",
+					"spring.cloud.gateway.routes[0].filters[0]=" + "RewritePath=/testService/(?<path>.*), /$\\{path}",
+					"spring.cloud.gateway.routes[0].filters[1].name=" + "CircuitBreaker",
+					"spring.cloud.gateway.routes[0].filters[1].args.name=" + "backendA",
+					"spring.cloud.gateway.routes[0].filters[1].args.fallbackUri=" + "forward:/fallback/testService"
+			).applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 
-    @Autowired
-    TestRestTemplate template;
-    final Random random = new Random();
-    int i = 0;
+	@Rule
+	public TestRule benchmarkRun = new BenchmarkRule();
+	private MockServerClient client = new MockServerClient(mockServerContainer.getContainerIpAddress(), mockServerContainer.getServerPort());
+	@Autowired
+	private TestRestTemplate template;
+	private int testPathSelector = 0;
 
-    @BeforeClass
-    public static void init() {
-        // override the routes with the circuit breaker filter
-        System.setProperty("spring.cloud.gateway.routes[0].id", "test-service-withResilient4j");
-        System.setProperty("spring.cloud.gateway.routes[0].uri", "http://192.168.99.100:" + mockServer.getServerPort());
-        System.setProperty("spring.cloud.gateway.routes[0].predicates[0]", "Path=/testService/**");
-        System.setProperty("spring.cloud.gateway.routes[0].filters[0]", "RewritePath=/testService/(?<path>.*), /$\\{path}");
-        System.setProperty("spring.cloud.gateway.routes[0].filters[1].name", "CircuitBreaker");
-        System.setProperty("spring.cloud.gateway.routes[0].filters[1].args.name", "backendA");
-        System.setProperty("spring.cloud.gateway.routes[0].filters[1].args.fallbackUri", "forward:/fallback/testService");
-        MockServerClient client = new MockServerClient(mockServer.getContainerIpAddress(), mockServer.getServerPort());
-        client.when(HttpRequest.request()
-                .withPath("/1"))
-                .respond(response()
-                        .withBody("{\"msgCode\":\"1\",\"msg\":\"1234567890\"}")
-                        .withHeader("Content-Type", "application/json"));
-        client.when(HttpRequest.request()
-                .withPath("/2"), Times.exactly(5))
+	@Before
+	public void init() {
+		client.when(HttpRequest.request()
+				.withPath("/1"))
 				.respond(response()
-                        .withBody("{\"msgCode\":\"2\",\"msg\":\"1234567891\"}")
-                        .withDelay(TimeUnit.MILLISECONDS, 200)
-                        .withHeader("Content-Type", "application/json"));
-        client.when(HttpRequest.request()
-                .withPath("/2"))
-                .respond(response()
-                        .withBody("{\"msgCode\":\"2\",\"msg\":\"1234567891\"}")
-                        .withHeader("Content-Type", "application/json"));
-    }
+						.withBody("{\"msgCode\":\"1\",\"msg\":\"1234567890\"}")
+						.withHeader("Content-Type", "application/json"));
+		client.when(HttpRequest.request()
+				.withPath("/2"), Times.exactly(5))
+				.respond(response()
+						.withBody("{\"msgCode\":\"2\",\"msg\":\"1234567891\"}")
+						.withDelay(TimeUnit.MILLISECONDS, 200)
+						.withHeader("Content-Type", "application/json"));
+		client.when(HttpRequest.request()
+				.withPath("/2"))
+				.respond(response()
+						.withBody("{\"msgCode\":\"2\",\"msg\":\"1234567891\"}")
+						.withHeader("Content-Type", "application/json"));
+	}
 
-    @Test
-    @BenchmarkOptions(warmupRounds = 0, concurrency = 1, benchmarkRounds = 200)
-    public void testAccountService() {
-        int gen = 1 + (i++ % 2);
-        ResponseEntity<FallbackResponse> r = template.exchange("/testService/{id}", HttpMethod.GET, null, FallbackResponse.class, gen);
-        LOGGER.info("{}. Received: status->{}, payload->{}, call->{}", i, r.getStatusCodeValue(), r.getBody(), gen);
-    }
+	@Test
+	@BenchmarkOptions(warmupRounds = 0, concurrency = 1, benchmarkRounds = 200)
+	public void testAccountService() {
+		int generatedPathValue = 1 + (testPathSelector++ % 2);
+		ResponseEntity<FallbackResponse> r = template.exchange("/testService/{id}", HttpMethod.GET, null, FallbackResponse.class, generatedPathValue);
+		LOGGER.info("{}. Received: status->{}, payload->{}, call->{}", generatedPathValue, r.getStatusCodeValue(), r.getBody(), generatedPathValue);
+	}
 
+	@AfterClass
+    public static void close(){
+	    mockServerContainer.close();
+    }
 }
