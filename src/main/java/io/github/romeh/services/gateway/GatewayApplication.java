7047b49d087554e00e60d25dc5e72668d2e093c0
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2019-12-26 10:18:24 +0100

    update readme

--- src/main/java/io/github/romeh/services/gateway/GatewayApplication.java
+++ src/main/java/io/github/romeh/services/gateway/GatewayApplication.java
@@ -3,22 +3,15 @@ package io.github.romeh.services.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@Bean
-	KeyResolver userKeyResolver() {
-		return exchange -> Mono.just("1");
 	}
 
 	@Bean
