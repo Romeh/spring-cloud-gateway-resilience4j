96466c42a450b1d0c8a1ba137a4c997461ecc5ce
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2020-05-25 17:41:58 +0200

    code cleanup

--- src/main/java/io/github/romeh/services/gateway/GatewayApplication.java
+++ src/main/java/io/github/romeh/services/gateway/GatewayApplication.java
@@ -5,7 +5,6 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.context.annotation.Bean;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
@@ -20,25 +19,17 @@ public class GatewayApplication {
 	}
 
 	@Bean
-	public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry) {
+	public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry, TimeLimiterRegistry timeLimiterRegistry) {
 		ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new ReactiveResilience4JCircuitBreakerFactory();
-		reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
-		return reactiveResilience4JCircuitBreakerFactory;
-	}
-
-	@Bean
-	public Resilience4JCircuitBreakerFactory resilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry, TimeLimiterRegistry timeLimiterRegistry) {
-		Resilience4JCircuitBreakerFactory resilience4JCircuitBreakerFactory = new Resilience4JCircuitBreakerFactory();
 		// inject the created spring managed bean circuit breaker registry will all externally configured CBs
-		resilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+		reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
 		// Inject the the created spring managed bean time limter config for specific backend name otherwise use the default configuration from resilience4j
-		resilience4JCircuitBreakerFactory.configure(
+		reactiveResilience4JCircuitBreakerFactory.configure(
 				builder -> builder
 						.timeLimiterConfig(timeLimiterRegistry.getConfiguration("backendB").orElse(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(300)).build()))
 						.circuitBreakerConfig(circuitBreakerRegistry.getConfiguration("backendB").orElse(circuitBreakerRegistry.getDefaultConfig())),
 				"backendB");
-		return resilience4JCircuitBreakerFactory;
+		return reactiveResilience4JCircuitBreakerFactory;
 	}
-
 
 }
